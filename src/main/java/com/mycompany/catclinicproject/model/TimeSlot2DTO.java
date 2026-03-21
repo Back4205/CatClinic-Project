df9c223970d7af0b8ac3e89001b403c703a3c1e4
Giann Lương Sơn <glson0318@gmail.com>
2026-03-21 23:08:44 +0700

    update inter3

---
+++ src/main/java/com/mycompany/catclinicproject/model/TimeSlot2DTO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.catclinicproject.model;
+
+/**
+ *
+ * @author Son
+ */
+public class TimeSlot2DTO {
+    
+}
